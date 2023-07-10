931b6d23a5ab853848ac26bb143ee551b7f6e162
Edgar Kalinovski <36729677+EdgeSan@users.noreply.github.com>
2023-07-10 16:04:19 +0300

    Edit project.go function
    The function has been changed so it could accept paging. It's important when there are more than 1000 projects in the organization.

--- azure-devops-client/project.go
+++ azure-devops-client/project.go
@@ -24,30 +24,64 @@ type Project struct {
 	RepositoryList RepositoryList
 }
 
-func (c *AzureDevopsClient) ListProjects() (list ProjectList, error error) {
+func (c *AzureDevopsClient) ListProjects() (list ProjectList, err error) {
 	defer c.concurrencyUnlock()
 	c.concurrencyLock()
 
-	url := fmt.Sprintf(
-		"_apis/projects?$top=%v&api-version=%v",
-		c.LimitProject,
+	// Retrieve the first 1000 projects
+	firstBatchURL := fmt.Sprintf(
+		"_apis/projects?$top=1000&api-version=%v",
 		url.QueryEscape(c.ApiVersion),
 	)
-	response, err := c.rest().R().Get(url)
-	if err := c.checkResponse(response, err); err != nil {
-		error = err
-		return
+	firstBatchResponse, err := c.rest().R().Get(firstBatchURL)
+	if err := c.checkResponse(firstBatchResponse, err); err != nil {
+		return list, err
 	}
 
-	err = json.Unmarshal(response.Body(), &list)
+	var firstBatch ProjectList
+	err = json.Unmarshal(firstBatchResponse.Body(), &firstBatch)
 	if err != nil {
-		error = err
-		return
+		return list, err
+	}
+
+	list.Count = firstBatch.Count
+	list.List = append(list.List, firstBatch.List...)
+
+	// Fetch the remaining projects, if any
+	fetchedProjects := firstBatch.Count
+	for int64(fetchedProjects) < c.LimitProject {
+		remainingProjectsURL := fmt.Sprintf(
+			"_apis/projects?$top=%v&$skip=%v&api-version=%v",
+			1000,
+			fetchedProjects,
+			url.QueryEscape(c.ApiVersion),
+		)
+
+		remainingProjectsResponse, err := c.rest().R().Get(remainingProjectsURL)
+		if err := c.checkResponse(remainingProjectsResponse, err); err != nil {
+			return list, err
+		}
+
+		var remainingProjects ProjectList
+		err = json.Unmarshal(remainingProjectsResponse.Body(), &remainingProjects)
+		if err != nil {
+			return list, err
+		}
+
+		remainingProjectsCount := remainingProjects.Count
+		if remainingProjectsCount == 0 {
+			break // No more projects to fetch
+		}
+
+		list.Count += remainingProjectsCount
+		list.List = append(list.List, remainingProjects.List...)
+
+		fetchedProjects += remainingProjectsCount
 	}
 
 	for key, project := range list.List {
 		list.List[key].RepositoryList, _ = c.ListRepositories(project.Id)
 	}
 
-	return
+	return list, nil
 }
